ec57b5ab09953a3243b52c5dff3cecce969420ee
yamako <j9@ninja.local>
2017-10-05 23:13:24 +0900

    first commit

---
+++ RecursiveDo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RecursiveDo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
